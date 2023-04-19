4a01886867cfb9d6a6521081f6c61876223e445e
mick <mick.the.linux.geek@hotmail.com>
2023-04-19 16:24:44 -0400

    Add Dash meta_tags for mobile responsiveness

--- Earthquakes_v1.py
+++ Earthquakes_v1.py
@@ -88,7 +88,9 @@ geo_df = geo_df.copy()
 
 blackbold = {'color': 'black', 'font-weight': 'bold'}
 
-app = Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
+app = Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP],
+           meta_tags=[{'name': 'viewport',
+                       'content': 'width=device-width, initial-scale=1.0, maximum-scale=1.2, minimum-scale=0.5,'}])
 server = app.server
 
 app.layout = html.Div(
